d1db6b4d4deb1da94225971f139ef16b16807e3e
daulenskript <kazbekok7@gmail.com>
2025-02-07 00:37:03 +0600

    first commit

---
+++ src/main/java/com/manasmiracle/demo/ManasaiApplication.java
@@ -0,0 +1,13 @@
+package com.manasmiracle.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManasaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManasaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manasmiracle/demo/ManasaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manasmiracle.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManasaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
