1476dd2ea001329c1e9e6e4e0e42ef67ba5f4a91
daulenskript <kazbekok7@gmail.com>
2025-02-09 22:56:00 +0600

    added answer bar

---
+++ src/main/java/com/manasmiracle/demo/ChatAiController.java
@@ -0,0 +1,32 @@
+package com.manasmiracle.demo;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class ChatAiController {
+    private final ChatClient chatClient;
+
+
+    public ChatAiController(ChatClient.Builder chatClient) {
+        this.chatClient = chatClient.build();
+    }
+    @GetMapping("/{chat}")
+    public ResponseEntity<String> promptWithPathVariable(@PathVariable String chat) {
+        try {
+            String response = chatClient
+                    .prompt(chat)
+                    .call()
+                    .content();
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        }
+    }
+}
