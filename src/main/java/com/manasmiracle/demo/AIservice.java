9eae360b482d1fe653adc9e2a8ea91925f0af3dc
daulenskript <kazbekok7@gmail.com>
2025-04-19 00:48:12 +0600

    comment

--- src/main/java/com/manasmiracle/demo/AIservice.java
+++ src/main/java/com/manasmiracle/demo/AIservice.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AIservice {
+    //const
     private final static String APIKEY = "sk-7f7a293c784740fc890e453975c04f94";
     private final static String BASEURL = "https://api.deepseek.com";
 
